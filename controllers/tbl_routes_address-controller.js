e29d8ec89dfc98e7051c142397ad7bdfc3eb7f02
Ahmed Raza <razagis105@gmail.com>
2024-03-16 02:12:04 +0500

    routes improved

--- controllers/tbl_routes_address-controller.js
+++ controllers/tbl_routes_address-controller.js
@@ -1,102 +1,107 @@
-const { Op } = require("sequelize");
-const { tbl_route_addresses,users,tbl_routes } = require("../models");
-const log = require("../utils/logger");
+const { Op } = require('sequelize');
+const { tbl_route_addresses, users, tbl_routes } = require('../models');
+const log = require('../utils/logger');
 
 const createRouteAddress = async (body) => {
-  
   try {
-    if(body.length == 0){
-      return{
+    if (body.length == 0) {
+      return {
         status: 400,
-        message: "No route address found",
-      }
+        message: 'No route address found',
+      };
     }
     // find tbl_route_addresses by route_id
     let routeAddress = await tbl_route_addresses.findAll({
       where: { route_id: body[0].route_id },
-    })
+    });
 
-    if(routeAddress.length > 0){
+    if (routeAddress.length > 0) {
       // delete all route addresses
       await tbl_route_addresses.destroy({
         where: { route_id: body[0].route_id },
-      })
+      });
     }
     // create new route addresses
 
     let createdRouteAddress = await tbl_route_addresses.bulkCreate(body);
-    return{
-      status: 200,
-      message: "Route Address created successfully",
-      routeAddress: createdRouteAddress
-    }
-  } catch (error)
-   {
-    log.info(error)
     return {
-        status: 500,
-        message: "Something went wrong",
-      };
-   }
+      status: 200,
+      message: 'Route Address created successfully',
+      routeAddress: createdRouteAddress,
+    };
+  } catch (error) {
+    log.info(error);
+    return {
+      status: 500,
+      message: 'Something went wrong',
+    };
+  }
 };
 
-const getAllRoutesAddresses = async (user) => {
-  
+const getAllRoutesAddresses = async (user, status) => {
   let routeIds = [];
 
-  if(user.userType == "manager"){
+  if (user.userType == 'manager') {
     let companyAdmin = await users.findOne({
-      where : {
-        id : user.id
-      }
+      where: {
+        id: user.id,
+      },
     });
-    if(!companyAdmin) return {
-      status: 400,
-      message: "No routes found",
-    };
-    if(companyAdmin.dataValues.childOf){
+    if (!companyAdmin)
+      return {
+        status: 400,
+        message: 'No routes found',
+      };
+    if (companyAdmin.dataValues.childOf) {
       user.id = companyAdmin.dataValues.childOf;
     }
     let companyRoutes = await tbl_routes.findAll({
-      where : {
-        user_id : user.id
-      }
+      where: {
+        user_id: user.id,
+      },
     });
-    companyRoutes.forEach(route => {
+    companyRoutes.forEach((route) => {
       routeIds.push(route.dataValues.id);
     });
   }
 
-
   try {
-    let routeAddresses = await tbl_route_addresses.findAll({
-      // where addresss does not include undefined word in string
-      where: {
-        complete_address: {
-          [Op.notLike]: "%undefined%",
-        },
-        [user.userType == "manager" ? 'route_id' : 'geom']: user.userType == "manager" ? {
-          [Op.in] : routeIds
-        } : null
+    let whereClause = {
+      complete_address: {
+        [Op.notLike]: '%undefined%',
       },
+      [user.userType == 'manager' ? 'route_id' : 'geom']:
+        user.userType == 'manager'
+          ? {
+              [Op.in]: routeIds,
+            }
+          : null,
+    };
+
+    if (status) {
+      whereClause.status = status; // Add filter based on status
+    }
+
+    let routeAddresses = await tbl_route_addresses.findAll({
+      where: whereClause,
     });
 
-    if (!routeAddresses) {
+    if (!routeAddresses || routeAddresses.length === 0) {
       return {
         status: 400,
-        message: "No route address found",
+        message: 'No route addresses found',
       };
     } else {
       return {
         status: 200,
-        routeAddress: routeAddresses,
+        routeAddresses: routeAddresses,
       };
     }
   } catch (error) {
     log.info(error);
     return {
       status: 500,
-      message: "Something went wrong",
+      message: 'Something went wrong',
     };
   }
 };
@@ -106,24 +111,24 @@ const getRouteAddress = async (id) => {
     let routeAddress = await tbl_route_addresses.findAll({
       where: { route_id: id },
     });
-    if(!route){
-      return{
+    if (!route) {
+      return {
         status: 400,
-        message: "No route address found"
-      }
-    }else{
-      return{
+        message: 'No route address found',
+      };
+    } else {
+      return {
         status: 200,
-        message: "Route address found",
-        routeAddress: routeAddress
-      }
-    }  
+        message: 'Route address found',
+        routeAddress: routeAddress,
+      };
+    }
   } catch (error) {
- log.info(error)
- return {
-    status: 500,
-    message: "Something went wrong",
-  };
+    log.info(error);
+    return {
+      status: 500,
+      message: 'Something went wrong',
+    };
   }
 };
 
@@ -135,14 +140,14 @@ const updateRouteAddress = async (routeId, body) => {
     });
     return {
       status: 200,
-      message: "Route updated successfully",
+      message: 'Route updated successfully',
       routeAddress: updatedRouteAddress[1],
     };
   } catch (error) {
     log.info(error);
     return {
       status: 500,
-      message: "something went wrong",
+      message: 'something went wrong',
     };
   }
 };
@@ -155,23 +160,21 @@ const deleteRouteAddress = async (routeId) => {
     if (deletedRouteAddress == 0) {
       return {
         status: 400,
-        message: "Route Address not found",
+        message: 'Route Address not found',
       };
     }
     return {
       status: 200,
-      message: "Route address deleted successfully",
+      message: 'Route address deleted successfully',
     };
   } catch (error) {
     log.info(error);
     return {
-        status: 500,
-        message: "Something went wrong",
-      };
+      status: 500,
+      message: 'Something went wrong',
+    };
   }
 };
-
-
 
 module.exports = {
   createRouteAddress,
--- routes/tbl_routes_address-routes.js
+++ routes/tbl_routes_address-routes.js
@@ -1,17 +1,19 @@
 // express router
-const express = require("express");
-const loginController = require("../controllers/login-controller");
+const express = require('express');
+const loginController = require('../controllers/login-controller');
 const {
   createRouteAddress,
   getAllRoutesAddresses,
-  getRouteAddress, updateRouteAddress,
-  deleteRouteAddress } = require("../controllers/tbl_routes_address-controller");
+  getRouteAddress,
+  updateRouteAddress,
+  deleteRouteAddress,
+} = require('../controllers/tbl_routes_address-controller');
 const router = express.Router();
 // models
-const db = require("../models");
-const { sendErrorResp } = require("../utils/common-utils");
+const db = require('../models');
+const { sendErrorResp } = require('../utils/common-utils');
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   createRouteAddress(req.body)
     .then((result) => res.status(result.status).send(result))
     .catch((error) => {
@@ -19,31 +21,30 @@ router.post("/", async (req, res) => {
     });
 });
 
-
-router.get("/", async (req, res) => {
-  getAllRoutesAddresses(req.user)
+router.get('/', async (req, res) => {
+  getAllRoutesAddresses(req.user, req.query.status)
     .then((result) => res.status(result.status).send(result))
     .catch((error) => {
       sendErrorResp(error, req, res);
     });
 });
-router.get("/:id", async (req, res) => {
-  getRouteAdress(req.params.id)
-    .then((result) => res.status(result.status).send(result))
-    .catch((error) => {
-      sendErrorResp(error, req, res);
-    });
-});
-
-router.patch("/:id", async (req, res) => {
-  updateRouteAdress(req.params.id, req.body)
+router.get('/:id', async (req, res) => {
+  getRouteAddress(req.params.id)
     .then((result) => res.status(result.status).send(result))
     .catch((error) => {
       sendErrorResp(error, req, res);
     });
 });
 
-router.delete("/:id", async (req, res) => {
+router.patch('/:id', async (req, res) => {
+  updateRouteAddress(req.params.id, req.body)
+    .then((result) => res.status(result.status).send(result))
+    .catch((error) => {
+      sendErrorResp(error, req, res);
+    });
+});
+
+router.delete('/:id', async (req, res) => {
   deleteRouteAddress(req.params.id)
     .then((result) => res.status(result.status).send(result))
     .catch((error) => {
@@ -51,26 +52,28 @@ router.delete("/:id", async (req, res) => {
     });
 });
 
-router.post("/updateAddressPointStatus", async (req, res) => {
+router.post('/updateAddressPointStatus', async (req, res) => {
   try {
     let addressPoint = await db.tbl_route_addresses.findOne({
       where: { id: req.body.id },
     });
     if (!addressPoint) {
-      return res.status(400).send("Address point not found");
+      return res.status(400).send('Address point not found');
     }
     addressPoint.status = req.body.status;
-    await db.tbl_route_addresses.update({
-      status: req.body.status,
-    }, {
-      where: { id: req.body.id },
-    })
-    return res.status(200).send("Address point status updated successfully");
-  }
-  catch (error) {
+    await db.tbl_route_addresses.update(
+      {
+        status: req.body.status,
+      },
+      {
+        where: { id: req.body.id },
+      }
+    );
+    return res.status(200).send('Address point status updated successfully');
+  } catch (error) {
     console.log(error);
-    return res.status(500).send("Something went wrong");
+    return res.status(500).send('Something went wrong');
   }
-})
+});
 
 module.exports = router;
