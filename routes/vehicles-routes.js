71d38020c2df8862f4ed9ec96b161c112fc376dd
Ahmed Raza <razagis105@gmail.com>
2024-03-19 02:08:55 +0500

    minor fixes

--- routes/vehicles-routes.js
+++ routes/vehicles-routes.js
@@ -1,36 +1,36 @@
 // express router
-const express = require("express");
+const express = require('express');
 const {
   getAllVehicles,
   updateVehicle,
   deleteVehicle,
   getVehicle,
   createVehicle,
-  getVehiclesInfo
-} = require("../controllers/vehicle-controller");
+  getVehiclesInfo,
+} = require('../controllers/vehicle-controller');
 const router = express.Router();
 // models
-const db = require("../models");
-const { sendErrorResp } = require("../utils/common-utils");
+const db = require('../models');
+const { sendErrorResp } = require('../utils/common-utils');
+const { upload } = require('../middleware/multer');
 
-router.post("/", async (req, res) => {
-  createVehicle(req.body,req?.user)
+router.post('/', upload.any(), async (req, res) => {
+  createVehicle(req.body, req?.user)
     .then((result) => res.status(result.status).send(result))
     .catch((error) => {
       sendErrorResp(error, req, res);
     });
 });
 
-router.get("/", async (req, res) => {
-  getAllVehicles(req.query.page,req.query.sortBy,req.query.showing,req?.user)
+router.get('/', async (req, res) => {
+  getAllVehicles(req.query.page, req.query.sortBy, req.query.showing, req?.user)
     .then((result) => res.status(result.status).send(result))
     .catch((error) => {
       sendErrorResp(error, req, res);
     });
 });
 
-
-router.get("/info", async (req, res) => {
+router.get('/info', async (req, res) => {
   getVehiclesInfo(req?.user)
     .then((result) => res.status(result.status).send(result))
     .catch((error) => {
@@ -38,7 +38,7 @@ router.get("/info", async (req, res) => {
     });
 });
 
-router.patch("/:id", async (req, res) => {
+router.patch('/:id', async (req, res) => {
   updateVehicle(req.params.id, req.body)
     .then((result) => res.status(result.status).send(result))
     .catch((error) => {
@@ -46,7 +46,7 @@ router.patch("/:id", async (req, res) => {
     });
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   deleteVehicle(req.params.id, req.body)
     .then((result) => res.status(result.status).send(result))
     .catch((error) => {
